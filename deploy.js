bb818094f47f75ad290d6ca3774082156410c4c0
OneTruePeach <56085420+OneTruePeach@users.noreply.github.com>
2026-08-07 16:45:17 -0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -22,49 +22,63 @@ const client1Rest = new REST().setToken(process.env.MAIN_TOKEN);
 const client2Rest = new REST().setToken(process.env.AMBIENCE_TOKEN);
 
 (async() => {
+    console.log('Refreshing commands for main bot...');
     try {
-        console.log('Refreshing commands for main bot...');
         await client1Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_1, process.env.GUILD_ID_1),
             { body: client1Commands },
         );
-
+        console.log(`Main refreshed successfully for Guild: ${process.env.GUILD_ID_1}`);
+    } catch(error) {
+        console.error(error);
+    }
+    try {
         await client1Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_1, process.env.GUILD_ID_2),
             { body: client1Commands },
         );
-
+        console.log(`Main refreshed successfully for Guild: ${process.env.GUILD_ID_2}`);
+    } catch(error) {
+        console.error(error);
+    }
+    try {
         await client1Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_1, process.env.GUILD_ID_3),
             { body: client1Commands },
         );
-
-        console.log("Commands refreshed for main bot.");
+        console.log(`Main refreshed successfully for Guild: ${process.env.GUILD_ID_3}`);
     } catch(error) {
-        console.log(error);
+        console.error(error);
     }
 })();
 
 (async() => {
+    console.log('Refreshing commands for ambience bot...');
     try {
-        console.log('Refreshing commands for ambience bot...');
         await client2Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_2, process.env.GUILD_ID_1),
             { body: client2Commands },
         );
-
+        console.log(`Ambience refreshed successfully for Guild: ${process.env.GUILD_ID_1}`);
+    } catch(error) {
+        console.error(error);
+    }
+    try {
         await client2Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_2, process.env.GUILD_ID_2),
             { body: client2Commands },
         );
-
+        console.log(`Ambience refreshed successfully for Guild: ${process.env.GUILD_ID_2}`);
+    } catch(error) {
+        console.error(error);
+    }
+    try {
         await client2Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_2, process.env.GUILD_ID_3),
             { body: client2Commands },
         );
-
-        console.log("Commands refreshed for ambience bot.");
+        console.log(`Ambience refreshed successfully for Guild: ${process.env.GUILD_ID_3}`);
     } catch(error) {
-        console.log(error);
+        console.error(error);
     }
 })();
