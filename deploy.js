ef13577d259508eb5fc9a0a031ac39cfcda54944
OneTruePeach <56085420+OneTruePeach@users.noreply.github.com>
2026-08-07 16:48:06 -0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -29,21 +29,21 @@ const client2Rest = new REST().setToken(process.env.AMBIENCE_TOKEN);
             { body: client1Commands },
         );
         console.log(`Main refreshed successfully for Guild: ${process.env.GUILD_ID_1}`);
-    } catch(error) { continue; }
+    } catch(error) { }
     try {
         await client1Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_1, process.env.GUILD_ID_2),
             { body: client1Commands },
         );
         console.log(`Main refreshed successfully for Guild: ${process.env.GUILD_ID_2}`);
-    } catch(error) { continue; }
+    } catch(error) { }
     try {
         await client1Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_1, process.env.GUILD_ID_3),
             { body: client1Commands },
         );
         console.log(`Main refreshed successfully for Guild: ${process.env.GUILD_ID_3}`);
-    } catch(error) { continue; }
+    } catch(error) { }
 })();
 
 (async() => {
@@ -54,19 +54,19 @@ const client2Rest = new REST().setToken(process.env.AMBIENCE_TOKEN);
             { body: client2Commands },
         );
         console.log(`Ambience refreshed successfully for Guild: ${process.env.GUILD_ID_1}`);
-    } catch(error) { continue; }
+    } catch(error) { }
     try {
         await client2Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_2, process.env.GUILD_ID_2),
             { body: client2Commands },
         );
         console.log(`Ambience refreshed successfully for Guild: ${process.env.GUILD_ID_2}`);
-    } catch(error) { continue; }
+    } catch(error) { }
     try {
         await client2Rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID_2, process.env.GUILD_ID_3),
             { body: client2Commands },
         );
         console.log(`Ambience refreshed successfully for Guild: ${process.env.GUILD_ID_3}`);
-    } catch(error) { continue; }
+    } catch(error) { }
 })();
